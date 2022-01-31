a431e38c73f64b72ab5261b7fd1cd26a2eb827fc
WortBildTon <woWortBildTonrtbildton@Lasses-MacBook-Air.local>
2022-01-31 09:01:08 +0100

    new header

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
-
-function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
-}
-
-
-
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
